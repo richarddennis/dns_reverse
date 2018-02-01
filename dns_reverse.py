31d3ce06c103ec2ea7dfdff7c38e24938977eb34
Richard Dennis <richard@dragoninfosec.com>
2018-02-01 11:50:06 +0000

    Update dns_reverse.py

--- dns_reverse.py
+++ dns_reverse.py
@@ -6,8 +6,8 @@ import subprocess
 import pyasn
 
 yesterday = (datetime.datetime.now() - datetime.timedelta(days = 1)).strftime("%Y-%m-%d")
-file_name = "netstat_data_relay_Zetservers_one_"+yesterday+".json"
-ip_name = "netstat_data_relay_Zetservers_one_"+yesterday+".txt"
+file_name = "All_data.json"
+ip_name = "netstat_data_all_bridges.txt"
 
 with open('somefile.txt', 'a') as the_file:
     the_file.write("gawk -F"'" "'" '{print $8 }' " + file_name + "| sort >"+ ip_name)
@@ -16,7 +16,7 @@ command1 = "gawk -F"'" "'" '{print $8 }' " + file_name + "| sort >" +ip_name
 
 os.system(command1)
 
-ip_name_formatted = "netstat_data_relay_Zetservers_one_"+yesterday+"_formatted.txt"
+ip_name_formatted = "netstat_data_all_bridges_formatted.txt"
 
 
 with open(ip_name, 'r') as f, open(ip_name_formatted, 'w') as n:
@@ -25,7 +25,7 @@ with open(ip_name, 'r') as f, open(ip_name_formatted, 'w') as n:
         n.write(new_line+'\n')
 
 
-dns_records = "netstat_data_relay_Zetservers_one_"+yesterday+"_dns_records.txt"
+dns_records = "netstat_data_all_bridges_dns_records.txt"
 
 with open(ip_name_formatted, 'r') as f, open(dns_records, 'w') as n:
     for line in f:
@@ -42,7 +42,7 @@ with open(ip_name_formatted, 'r') as f, open(dns_records, 'w') as n:
             # sys.exit()
         # n.write(new_line+'\n')
 
-asn_records = "netstat_data_relay_Zetservers_one_"+yesterday+"_asn_records.txt"
+asn_records = "netstat_data_all_bridges_asn_records.txt"
 
 with open(ip_name_formatted, 'r') as f, open(asn_records, 'w') as n:
     asndb = pyasn.pyasn('ipasn_20140513.dat')
