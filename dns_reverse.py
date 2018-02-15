8d06b225af85dee47dd4dd0f9fa76a7c36a7a9bd
Richard Dennis <richard@dragoninfosec.com>
2018-02-15 13:13:15 +0000

    Update dns_reverse.py

--- dns_reverse.py
+++ dns_reverse.py
@@ -5,29 +5,29 @@ import sys
 import subprocess
 import pyasn
 
-yesterday = (datetime.datetime.now() - datetime.timedelta(days = 1)).strftime("%Y-%m-%d")
-file_name = "All_data.json"
-ip_name = "netstat_data_all_bridges.txt"
+#yesterday = (datetime.datetime.now() - datetime.timedelta(days = 1)).strftime("%Y-%m-%d")
+#file_name = "All_data.json"
+#ip_name = "netstat_data_all_bridges.txt"
+#
+#with open('somefile.txt', 'a') as the_file:
+#    the_file.write("gawk -F"'" "'" '{print $8 }' " + file_name + "| sort >"+ ip_name)
+#
+#command1 = "gawk -F"'" "'" '{print $8 }' " + file_name + "| sort >" +ip_name
+#
+#os.system(command1)
+#
+#ip_name_formatted = "netstat_data_all_bridges_formatted.txt"
+#
+#
+#with open(ip_name, 'r') as f, open(ip_name_formatted, 'w') as n:
+#    for line in f:
+#        new_line = re.sub('[^a-zA-Z0-9_. ]+', ' ', line)
+#        n.write(new_line+'\n')
 
-with open('somefile.txt', 'a') as the_file:
-    the_file.write("gawk -F"'" "'" '{print $8 }' " + file_name + "| sort >"+ ip_name)
-
-command1 = "gawk -F"'" "'" '{print $8 }' " + file_name + "| sort >" +ip_name
-
-os.system(command1)
-
-ip_name_formatted = "netstat_data_all_bridges_formatted.txt"
-
-
-with open(ip_name, 'r') as f, open(ip_name_formatted, 'w') as n:
-    for line in f:
-        new_line = re.sub('[^a-zA-Z0-9_. ]+', ' ', line)
-        n.write(new_line+'\n')
-
-
+input_data_file = "no_dup_ip.txt"
 dns_records = "netstat_data_all_bridges_dns_records.txt"
 
-with open(ip_name_formatted, 'r') as f, open(dns_records, 'w') as n:
+with open(input_data_file, 'r') as f, open(dns_records, 'w') as n:
     for line in f:
         #print line.strip()
         if line.strip() == "0.0.0.0":
@@ -44,7 +44,7 @@ with open(ip_name_formatted, 'r') as f, open(dns_records, 'w') as n:
 
 asn_records = "netstat_data_all_bridges_asn_records.txt"
 
-with open(ip_name_formatted, 'r') as f, open(asn_records, 'w') as n:
+with open(input_data_file, 'r') as f, open(asn_records, 'w') as n:
     asndb = pyasn.pyasn('ipasn_20140513.dat')
     for line in f:
         #print line.strip()
